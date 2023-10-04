7fe6314db81cf10153bfc3aa606ca2f67681d153
hafizaatifkamal <hafizaatifkamal@gmail.com>
2023-10-04 13:39:32 +0530

    PORT added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const router = require("./routes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON body
 app.use(express.json());
